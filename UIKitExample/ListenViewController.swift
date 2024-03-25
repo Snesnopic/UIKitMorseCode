03f49bcce18a6da220ed1dedd5835f2e80537c2e
Snesnopic <snesnopic@gmail.com>
2024-03-25 11:07:10 +0100

    Created and connected view controllers

---
+++ UIKitExample/ListenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListenViewController.swift
+//  UIKitExample
+//
+//  Created by Giuseppe Francione on 25/03/24.
+//
+
+import UIKit
+
+class ListenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ UIKitExample/QuickTranslateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTranslateViewController.swift
+//  UIKitExample
+//
+//  Created by Giuseppe Francione on 25/03/24.
+//
+
+import UIKit
+
+class QuickTranslateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
