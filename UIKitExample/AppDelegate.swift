02756e56e1d1635a4eba9357578e85bbc193422f
Snesnopic <snesnopic@gmail.com>
2024-03-26 09:49:14 +0100

    Now checks for haptics compatibility at the start

--- UIKitExample/AppDelegate.swift
+++ UIKitExample/AppDelegate.swift
@@ -6,19 +6,25 @@
 //
 
 import UIKit
+import CoreHaptics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var supportsHaptics: Bool = false
+    
+    /// - Tag: CheckHapticCompatibility
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Check if the device supports haptics.
+        let hapticCapability = CHHapticEngine.capabilitiesForHardware()
+        supportsHaptics = hapticCapability.supportsHaptics
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
